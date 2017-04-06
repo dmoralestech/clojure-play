8d8f799c896fe4c2eef2b6e688e5ec2d3520a8ce
dmoralestech <darwin.morales.tech@gmail.com>
2017-04-06 19:12:01 +1000

    Update scheme.scm

--- src/clojure_app/scheme.scm
+++ src/clojure_app/scheme.scm
@@ -7,3 +7,11 @@
 (define (add x y) (+ x y) )
 
 ((op-maker add) 2 3)
+
+
+;; Accumulates the result of the first and the already-accumulated rest. 
+(define (accumulate op initial sequence) 
+(if (null? sequence) 
+   initial 
+   (op (car sequence) 
+       (accumulate op initial (cdr sequence))))) 
