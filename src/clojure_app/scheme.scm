61f6c5fcd8f275fcb66d748067b359acbbb8e48e
dmoralestech <darwin.morales.tech@gmail.com>
2017-04-07 20:31:08 +1000

    Update scheme.scm
    pascal triangle

--- src/clojure_app/scheme.scm
+++ src/clojure_app/scheme.scm
@@ -15,3 +15,10 @@
    initial 
    (op (car sequence) 
        (accumulate op initial (cdr sequence))))) 
+
+
+(define (pascal row col)
+   (cond ((< row col) #f)
+         ((or (= 0 col) (= row col)) 1)
+         (else (+ (pascal (- row 1) col)
+                  (pascal (- row 1) (- col 1))))))
